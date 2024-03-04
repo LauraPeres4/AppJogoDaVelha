981f227aea6d3c7ec4c791fbf653b3e9b2f199ed
Usuario <Usuario@CPD03-M06>
2024-03-04 15:45:38 -0300

    Adicionar arquivos de projeto.

---
+++ AppJogoDaVelha/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace AppJogoDaVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "O"
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+         
+    }
+
+}
